c4319809d13b9b3be0a784dbc4e1220cf4439ae6
Thomas <thomas@craftacademy.se>
2019-01-04 11:55:44 +0100

    green state for feature tests for BMI happy path

--- src/e2e.feature.js
+++ src/e2e.feature.js
@@ -1,9 +1,22 @@
-describe('Google', () => {
+describe('BMI Converter', () => {
     beforeAll(async () => {
-        await page.goto('https://craftacademy.co');
+        await page.goto('http://localhost:3000');
     });
 
-    it('should display "google" text on page', async () => {
-        await expect(page).toMatch('Craft Academy');
+    it('should display "BMI Converter" text on page', async () => {
+        await expect(page).toMatch('BMI Converter');
     });
+
+    it('calculates metric results', async () => {
+        await expect(page).toFill('input[name="weight"]', '95')
+        await expect(page).toFill('input[name="height"]', '186')
+        await expect(page).toMatch('You are Overweight with a BMI of 27.46')
+    })
+
+    it('calculates imperial results', async () => {
+        await expect(page).toSelect('select[name="method"]', 'Imperial')
+        await expect(page).toFill('input[name="weight"]', '200')
+        await expect(page).toFill('input[name="height"]', '73')
+        await expect(page).toMatch('You are Overweight with a BMI of 26.38')
+    })
 });
